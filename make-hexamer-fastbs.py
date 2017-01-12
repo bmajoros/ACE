5ad0dc0688bc20132bb26512f01f9edfb53ff293
Bill Majoros <martiandna@gmail.com>
2017-01-12 15:38:16 -0500

    make-hexamer-fastbs.py

---
+++ make-hexamer-fastbs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#=========================================================================
+# This is OPEN SOURCE SOFTWARE governed by the Gnu General Public
+# License (GPL) version 3, as described at www.opensource.org.
+# Copyright (C)2016 William H. Majoros (martiandna@gmail.com).
+#=========================================================================
+from __future__ import (absolute_import, division, print_function, 
+   unicode_literals, generators, nested_scopes, with_statement)
+from builtins import (bytes, dict, int, list, object, range, str, ascii,
+   chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
+# The above imports should allow this program to run in both Python 2 and
+# Python 3.  You might need to update your version of module "future".
+import sys
+import ProgramName
+
+if(len(sys.argv)!=3):
+    exit(ProgramName.get()+" <hexamers-and-weights.txt> <outdir>\n")
+(weightsFile,outDir)=sys.argv[1:]
+
+with open(weightsFile,"rt") as IN:
+    for line in IN:
+        fields=line.rstrip().split()
+        if(len(fields)!=2): continue
+        (hexamer,score)=fields
+        outfile=outDir+"/"+hexamer+".fastb"
+        OUT=open(outfile,"wt")
+        OUT.write(">dna\n"+hexamer+"\n")
+        OUT.close()
+
+
+
+
