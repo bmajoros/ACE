0d8a25a9b6a5144f2744e0f93305d0873deee55f
Bill Majoros <martiandna@gmail.com>
2017-04-25 11:41:11 -0400

    updated logistic-regression.R

--- logistic-regression.R
+++ logistic-regression.R
@@ -1,18 +1,21 @@
 #!/bin/env Rscript
 #    #!/usr/bin/Rscript --vanilla
+library(glmnet)
+library(methods)
 
 args <- commandArgs(TRUE)
-if(length(args)!=1) {
-  cat("usage: <feature-vectors.txt>\n");
+if(length(args)!=2) {
+  cat("usage: <feature-vectors.txt> <out-betas.txt>\n");
   q(status=1)
 }
-infile <- args[1];
+infile <- args[1]
+outfile <- args[2]
 
 data <- read.table(infile,header=T)
 y <- data$category
-x <- data[2:4097]
+x <- data[2:ncol(data)]
 X <- as.matrix(x)
-sink("coefficients-cv-binomial.txt")
+sink(outfile) # "coefficients-cv-binomial.txt"
 cvfit = cv.glmnet(X, y, family="binomial")
 coef(cvfit, s = "lambda.min")
 sink()
